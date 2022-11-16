d9eb8a8db120c0adc250b17378cc0a2ce9bf58ec
adricosmicfox <adriiphone@gmail.com>
2022-11-16 17:10:46 +0100

    exercise completed

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -8,9 +8,16 @@
         <label class="" for="title">Titolo:</label>
         <input class="p-2" type="text" name="title" value='{{ old('title', '') }}' required maxlength="250"
             minòength="1" />
+
         <label class="" for="content">Content:</label>
         <textarea type="text" name="content" minlength="1" rows="30" cols="70" class="p-4">{{ old('content', '') }}</textarea>
 
+        <label for="category_id">Category:</label>
+        <select name="catgory_id" id="">
+            @foreach ($categories as $category)
+                <option value="{{ $category->id }}">"{{ $category->name }}"</option>
+            @endforeach
+        </select>
 
         <div class=" pt-5 d-flex justify-content-center">
             <input type="submit" class="btn btn-success" value="Registra">
