3c85f43dd5cd36978d4d2638d0f3168e9b1b557b
adricosmicfox <adriiphone@gmail.com>
2022-11-16 16:28:47 +0100

    done composer and npm

--- public/js/front.js
+++ public/js/front.js
@@ -49830,7 +49830,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\PHP\laravel-auth\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\PHP\laravel-one-to-many\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
