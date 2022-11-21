0ab10b3b09fbab0eb651ae0c914a6bca50a9b234
adricosmicfox <adriiphone@gmail.com>
2022-11-21 16:02:49 +0100

    created project and key

--- public/js/front.js
+++ public/js/front.js
@@ -49830,7 +49830,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\PHP\laravel-many-to-many\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\PHP\laravel-api\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
